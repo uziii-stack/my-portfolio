dbb1f4620e49344cb666462e1e4970b3da6b1cb5
uziii-stack <uzairbaig040@gmail.com>
2025-11-30 17:29:06 +0500

    Add dist for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/",      // <<--- IMPORTANT
   plugins: [tailwindcss(), react()],
-  base: '/my-portfolio/', // ⭐ REQUIRED for GitHub Pages
 })
