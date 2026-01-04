9ed103dd09913f6767275ec017665b350f482f7f
uziii-stack <uzairbaig040@gmail.com>
2026-01-05 03:15:03 +0500

    Fix base path for GitHub Pages + Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from "vite";
+// vite.config.js
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: import.meta.env.VITE_BASE_PATH || "/",
-  plugins: [tailwindcss(), react()],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  // Check for Netlify or GitHub Pages
+  let base;
+
+  if (env.VITE_BASE_PATH) {
+    base = env.VITE_BASE_PATH; // from Netlify env variable
+  } else if (process.env.GITHUB_ACTIONS) {
+    base = "/my-portfolio/"; // GitHub Pages
+  } else {
+    base = "/"; // local dev
+  }
+
+  return defineConfig({
+    base,
+    plugins: [tailwindcss(), react()],
+  });
+};
