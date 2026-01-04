201add8c1a2af40f61b6e3ad75ebb567236333b7
uziii-stack <uzairbaig040@gmail.com>
2026-01-05 03:10:28 +0500

    deployement environment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/my-portfolio/",
+  base: import.meta.env.VITE_BASE_PATH || "/",
   plugins: [tailwindcss(), react()],
 });
